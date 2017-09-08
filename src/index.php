4f9f654c49fe5f7f6d7def90b1bde510554523cf
William Dale <william.dale@utiliware.com>
2017-09-07 22:28:04 -0700

    Updated

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Pardeep.... But I am learning so much!</h1>
+                <h1>Bill.... But I am learning so much!</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
